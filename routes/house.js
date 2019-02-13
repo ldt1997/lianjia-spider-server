9b56c4da6a78edca0bfc0c1eadc7abbc95d4135e
luodantong <474376875@qq.com>
2019-02-14 00:16:06 +0800

    初步完成house页面接口（除柱状图）

--- routes/house.js
+++ routes/house.js
@@ -12,7 +12,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 /****爬虫服务器后台接口*****/
 
 /****house页面操作*****/
-//  获取概览数据
+/**获取概览数据**/
 router.post("/house/searchOverviewData", urlencodedParser, function(req, res) {
   console.log("获取概览数据", req.body.position);
   //请求成功
@@ -58,6 +58,7 @@ router.post("/house/searchOverviewData", urlencodedParser, function(req, res) {
         .toArray(function(err, result) {
           if (err) throw err;
           ep.emit("getTotalPrice", result);
+          db.close();
         });
     });
     ep.all(
@@ -78,6 +79,178 @@ router.post("/house/searchOverviewData", urlencodedParser, function(req, res) {
         });
       }
     );
+  }
+  //请求失败
+  else {
+    res.send({
+      data: {},
+      errorCode: "1", //0表示成功
+      errorMsg: "请求失败"
+    });
+  }
+});
+
+/**获取表格数据**/
+router.post("/house/searchTableData", urlencodedParser, function(req, res) {
+  console.log("获取表格数据", req.body.position);
+  //请求成功
+  if (req.body && req.body.position) {
+    const colName = req.body.position; // 表名
+    const { currentPage = 1 } = req.body; // 当前页数(默认第一页)
+    const { pageSize = 10 } = req.body; //页数显示数据条数（默认10条）
+    const skipNum = (currentPage - 1) * pageSize; //跳过的数据条数
+    // 连接数据库
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("lianjiaSpider"); //数据库名
+      //获取房源总数
+      dbo
+        .collection(colName)
+        .aggregate([{ $group: { _id: null, count: { $sum: 1 } } }])
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getHouseNum", result);
+        });
+      //获取表格数据
+      dbo
+        .collection(colName)
+        .find()
+        .skip(skipNum)
+        .limit(pageSize)
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getTableData", result);
+          db.close();
+        });
+    });
+    ep.all("getHouseNum", "getTableData", function(data1, data2) {
+      res.send({
+        data: {
+          houseNum: data1[0].count ? data1[0].count : null, //房源数量(int)
+          houseList: data2[0] ? data2 : [] //表格数据(float)
+        },
+        errorCode: "0", //0表示成功
+        errorMsg: ""
+      });
+    });
+  }
+  //请求失败
+  else {
+    res.send({
+      data: {},
+      errorCode: "1", //0表示成功
+      errorMsg: "请求失败"
+    });
+  }
+});
+
+/**获取饼图数据**/
+router.post("/house/searchDonutData", urlencodedParser, function(req, res) {
+  console.log("获取饼图数据", req.body.position);
+  //请求成功
+  if (req.body && req.body.position && req.body.type) {
+    const colName = req.body.position;
+    // 连接数据库
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("lianjiaSpider"); //数据库名
+      switch (req.body.type) {
+        case 1:
+          dbo
+            .collection(colName)
+            .aggregate([
+              { $match: { name: { $ne: NaN }, layout: { $ne: NaN } } },
+              { $group: { _id: "$name", count: { $sum: 1 } } }
+            ])
+            .toArray(function(err, result) {
+              if (err) throw err;
+              ep.emit("getDonutData", result);
+              db.close();
+            });
+          break;
+        case 2:
+          dbo
+            .collection(colName)
+            .aggregate([
+              {
+                $match: {
+                  name: { $ne: NaN },
+                  layout: { $ne: NaN, $ne: "车位" }
+                }
+              },
+              { $group: { _id: "$layout", count: { $sum: 1 } } }
+            ])
+            .toArray(function(err, result) {
+              if (err) throw err;
+              ep.emit("getDonutData", result);
+              db.close();
+            });
+          break;
+        default:
+          dbo
+            .collection(colName)
+            .aggregate([
+              { $match: { name: { $ne: NaN }, layout: { $ne: NaN } } },
+              { $group: { _id: "$name", count: { $sum: 1 } } }
+            ])
+            .toArray(function(err, result) {
+              if (err) throw err;
+              ep.emit("getDonutData", result);
+              db.close();
+            });
+      }
+    });
+    ep.all("getDonutData", function(data1) {
+      res.send({
+        data: {
+          filterData: data1[0] ? data1 : null
+        },
+        errorCode: "0", //0表示成功
+        errorMsg: ""
+      });
+    });
+  }
+  //请求失败
+  else {
+    res.send({
+      data: {},
+      errorCode: "1", //0表示成功
+      errorMsg: "请求失败"
+    });
+  }
+});
+
+/**获取排名数据（成交总价）**/
+router.post("/house/searchRankData", urlencodedParser, function(req, res) {
+  console.log("获取排名数据", req.body.position);
+  //请求成功
+  if (req.body && req.body.position) {
+    const colName = req.body.position; // 表名
+    // 连接数据库
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("lianjiaSpider"); //数据库名
+      //获取房源总数
+      dbo
+        .collection(colName)
+        .find({ totalPrice: { $ne: NaN } })
+        .sort({ totalPrice: -1 })
+        .limit(10)
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getRankData", result);
+          db.close();
+        });
+    });
+    ep.all("getRankData", function(data1) {
+      res.send({
+        data: {
+          filterData: data1[0] ? data1 : null //rank
+        },
+        errorCode: "0", //0表示成功
+        errorMsg: ""
+      });
+    });
   }
   //请求失败
   else {
