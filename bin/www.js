5e4afb797f5d3441b81c3c263e2b0a2c68c30b90
luodantong <474376875@qq.com>
2019-02-15 20:36:18 +0800

    完成house数据概览接口

---
+++ bin/www.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var houseRouter = require("../routes/house");
+
+//设置中间件读取请求中json数据
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//调用router中间件
+app.use("/", houseRouter);
+
+var server = app.listen(8090, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("应用实例，访问地址为 http://%s:%s", host, port);
+});
---
+++ routes/house.js
@@ -0,0 +1,92 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+var eventproxy = require("eventproxy"); //流程控制
+var ep = eventproxy();
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/";
+
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/****爬虫服务器后台接口*****/
+
+/****house页面操作*****/
+//  获取概览数据
+router.post("/house/searchOverviewData", urlencodedParser, function(req, res) {
+  console.log("获取概览数据", req.body.position);
+  //请求成功
+  if (req.body && req.body.position) {
+    const colName = req.body.position;
+    // 连接数据库
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("lianjiaSpider"); //数据库名
+      dbo
+        .collection(colName)
+        .aggregate([{ $group: { _id: null, count: { $sum: 1 } } }])
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getHouseNum", result);
+        });
+      dbo
+        .collection(colName)
+        .aggregate([
+          { $match: { unitPrice: { $ne: NaN } } },
+          { $group: { _id: null, count: { $avg: "$unitPrice" } } }
+        ])
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getAvgUnitPrice", result);
+        });
+      dbo
+        .collection(colName)
+        .aggregate([
+          { $match: { listedPrice: { $ne: NaN } } },
+          { $group: { _id: null, count: { $avg: "$listedPrice" } } }
+        ])
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getListedPrice", result);
+        });
+      dbo
+        .collection(colName)
+        .aggregate([
+          { $match: { totalPrice: { $ne: NaN } } },
+          { $group: { _id: null, count: { $avg: "$totalPrice" } } }
+        ])
+        .toArray(function(err, result) {
+          if (err) throw err;
+          ep.emit("getTotalPrice", result);
+        });
+    });
+    ep.all(
+      "getHouseNum",
+      "getAvgUnitPrice",
+      "getListedPrice",
+      "getTotalPrice",
+      function(data1, data2, data3, data4) {
+        res.send({
+          data: {
+            houseNum: data1[0].count ? data1[0].count : null, //房源数量(int)
+            avgUnitPrice: data2[0].count ? data2[0].count : null, //平均单价(float)
+            avgListedPrice: data3[0].count ? data3[0].count : null, //平均挂牌总价(float)
+            avgTotalPrice: data4[0].count ? data4[0].count : null //平均成交总价(float)
+          },
+          errorCode: "0", //0表示成功
+          errorMsg: ""
+        });
+      }
+    );
+  }
+  //请求失败
+  else {
+    res.send({
+      data: {},
+      errorCode: "1", //0表示成功
+      errorMsg: "请求失败"
+    });
+  }
+});
+
+module.exports = router;
